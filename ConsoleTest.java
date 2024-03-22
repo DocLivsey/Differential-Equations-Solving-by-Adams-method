ec58d19e2f0d45dd2cc89527505192d06e1aba51
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-03-23 02:53:28 +0300

    complete implicit adams method all examples works perfectly (but first example a little bit bad) start work on read data from configs file

--- ConsoleTest.java
+++ ConsoleTest.java
@@ -4,62 +4,53 @@ import java.io.*;
 
 public class ConsoleTest {
     public static void main(String[] args) throws IOException {
-        numericalAdamsMethodFifthTest();
+        String pathToParametersFile = "diffEquationSettingFile.txt";
+        numericalAdamsMethodSecondTest(pathToParametersFile);
     }
-    public static void numericalAdamsMethodFirstTest() throws IOException {
+    public static void numericalAdamsMethodFirstTest(String pathToParametersFile) throws IOException {
         String pathToImplicitFuncPoints = "pointsForImplicitFuncInput.txt";
         Point2D initCondition = Point2D.readPointFromFile(pathToImplicitFuncPoints);
         MathImplicitFunction implFunction = (x) -> new PointMultiD(x,
                 1 / (Math.sqrt(Math.pow(x.getItem(0), 2))));
-        MathImplicitFunctionOperations implicitFunction = new MathImplicitFunctionOperations(2,
-                pathToImplicitFuncPoints, implFunction);
-        implicitFunction.printPoints();
-        DifferentialEquation diffEquation = new DifferentialEquation(implicitFunction, initCondition);
-        diffEquation.implicitAdamsMethod(null);
+        DifferentialEquation diffEquation = new DifferentialEquation(
+                pathToImplicitFuncPoints, initCondition, implFunction);
+        diffEquation.implicitAdamsMethod(pathToParametersFile);
     }
-    public static void numericalAdamsMethodSecondTest() throws IOException {
+    public static void numericalAdamsMethodSecondTest(String pathToParametersFile) throws IOException {
         String pathToImplicitFuncPoints = "pointsForImplicitFuncInput.txt";
         Point2D initCondition = Point2D.readPointFromFile(pathToImplicitFuncPoints);
         MathImplicitFunction implFunction = (x) -> new PointMultiD(x,
                 (x.getItem(1) / x.getItem(0)) + x.getItem(0) * Math.cos(x.getItem(0)));
-        MathImplicitFunctionOperations implicitFunction = new MathImplicitFunctionOperations(2,
-                pathToImplicitFuncPoints, implFunction);
-        implicitFunction.printPoints();
-        DifferentialEquation diffEquation = new DifferentialEquation(implicitFunction, initCondition);
-        diffEquation.implicitAdamsMethod(null);
+        DifferentialEquation diffEquation = new DifferentialEquation(
+                pathToImplicitFuncPoints, initCondition, implFunction);
+        diffEquation.implicitAdamsMethod(pathToParametersFile);
     } // ФИЛИППОВ 141
-    public static void numericalAdamsMethodThirdTest() throws IOException {
+    public static void numericalAdamsMethodThirdTest(String pathToParametersFile) throws IOException {
         String pathToImplicitFuncPoints = "pointsForImplicitFuncInput.txt";
         Point2D initCondition = Point2D.readPointFromFile(pathToImplicitFuncPoints);
         MathImplicitFunction implFunction = (x) -> new PointMultiD(x,
                 Math.pow(x.getItem(0), 2) + 1 + (2*x.getItem(0)*x.getItem(1))
                         / (1 + Math.pow(x.getItem(0), 2)));
-        MathImplicitFunctionOperations implicitFunction = new MathImplicitFunctionOperations(2,
-                pathToImplicitFuncPoints, implFunction);
-        implicitFunction.printPoints();
-        DifferentialEquation diffEquation = new DifferentialEquation(implicitFunction, initCondition);
-        diffEquation.implicitAdamsMethod(null);
+        DifferentialEquation diffEquation = new DifferentialEquation(
+                pathToImplicitFuncPoints, initCondition, implFunction);
+        diffEquation.implicitAdamsMethod(pathToParametersFile);
     }
-    public static void numericalAdamsMethodFourthTest() throws IOException {
+    public static void numericalAdamsMethodFourthTest(String pathToParametersFile) throws IOException {
         String pathToImplicitFuncPoints = "pointsForImplicitFuncInput.txt";
         Point2D initCondition = Point2D.readPointFromFile(pathToImplicitFuncPoints);
         MathImplicitFunction implFunction = (x) -> new PointMultiD(x,
                 x.getItem(1) + 2 * x.getItem(0) - 3);
-        MathImplicitFunctionOperations implicitFunction = new MathImplicitFunctionOperations(2,
-                pathToImplicitFuncPoints, implFunction);
-        implicitFunction.printPoints();
-        DifferentialEquation diffEquation = new DifferentialEquation(implicitFunction, initCondition);
-        diffEquation.implicitAdamsMethod(null);
+        DifferentialEquation diffEquation = new DifferentialEquation(
+                pathToImplicitFuncPoints, initCondition, implFunction);
+        diffEquation.implicitAdamsMethod(pathToParametersFile);
     } // ФИЛИППОВ 63
-    public static void numericalAdamsMethodFifthTest() throws IOException {
+    public static void numericalAdamsMethodFifthTest(String pathToParametersFile) throws IOException {
         String pathToImplicitFuncPoints = "pointsForImplicitFuncInput.txt";
         Point2D initCondition = Point2D.readPointFromFile(pathToImplicitFuncPoints);
         MathImplicitFunction implFunction = (x) -> new PointMultiD(x,
                 1 / (x.getItem(0) + 2 * x.getItem(1)));
-        MathImplicitFunctionOperations implicitFunction = new MathImplicitFunctionOperations(2,
-                pathToImplicitFuncPoints, implFunction);
-        implicitFunction.printPoints();
-        DifferentialEquation diffEquation = new DifferentialEquation(implicitFunction, initCondition);
-        diffEquation.implicitAdamsMethod(null);
+        DifferentialEquation diffEquation = new DifferentialEquation(
+                pathToImplicitFuncPoints, initCondition, implFunction);
+        diffEquation.implicitAdamsMethod(pathToParametersFile);
     }
 }
--- DifferentialEquation.java
+++ DifferentialEquation.java
@@ -5,17 +5,72 @@ import LinearAlgebra.*;
 import java.io.*;
 import java.util.*;
 
-public class DifferentialEquation {
+public class DifferentialEquation extends MathBase {
+    protected final HashMap<String, Double> parametersTable = new HashMap<>();
+    protected double epsilon = 1E-5;
     protected MathImplicitFunctionOperations rightSideFunction; // F(x, y)
     protected MathFunctionOperations solutionFunction; // y(x)
     protected Point2D initialCondition; // y(x0) = y0 - The Cauchy`s problem
-    public DifferentialEquation (MathImplicitFunctionOperations rightSideFunction, Point2D initialCondition)
+    public DifferentialEquation(String pathToParametersFile, String pathToPoints3D, MathImplicitFunction function,
+                                ArrayList<PointMultiD> points3D, Point2D initialCondition,
+                                MathImplicitFunctionOperations rightSideFunction) throws IOException {
+        super.updateVariablesList(List.of("step of method", "x0", "y0"));
+        if (pathToParametersFile != null) {
+            this.setParametersTable(pathToParametersFile);
+            this.setEpsilon();
+        } else if (initialCondition != null && !initialCondition.isNullPoint() && this.initialCondition == null) {
+            this.initialCondition = initialCondition;
+        } else
+            throw new RuntimeException(PrettyOutput.ERROR +
+                    "ОШИБКА! Для решения задачи Коши необходимо задать начальные условия" + PrettyOutput.RESET);
+        if (rightSideFunction != null) {
+            this.rightSideFunction = rightSideFunction;
+        } else {
+            this.rightSideFunction = new MathImplicitFunctionOperations(
+                    pathToParametersFile, pathToPoints3D, points3D, 2, function);
+        }
+    }
+    public DifferentialEquation(String pathToParametersFile, String pathToPoints3D, Point2D initialCondition,
+                                MathImplicitFunction function) throws IOException {
+        this(pathToParametersFile, pathToPoints3D, function, null, initialCondition, null);
+    }
+    public DifferentialEquation(String pathToPoints3D, Point2D initialCondition,
+                                MathImplicitFunction function) throws IOException {
+        this(null, pathToPoints3D, initialCondition, function);
+    }
+    public DifferentialEquation(String pathToParametersFile, Point2D initialCondition, ArrayList<PointMultiD> points3D,
+                                MathImplicitFunction function) throws IOException {
+        this(pathToParametersFile, null, function, points3D, initialCondition, null);
+    }
+    public DifferentialEquation(Point2D initialCondition, ArrayList<PointMultiD> points3D,
+                                MathImplicitFunction function) throws IOException {
+        this(null, initialCondition, points3D, function);
+    }
+    public DifferentialEquation(String pathToParametersFile, MathImplicitFunctionOperations rightSideFunction,
+                                Point2D initialCondition) throws IOException {
+        this(pathToParametersFile, null, null, null, initialCondition, rightSideFunction);
+    }
+    public DifferentialEquation(MathImplicitFunctionOperations rightSideFunction,
+                                Point2D initialCondition) throws IOException {
+        this(null, rightSideFunction, initialCondition);
+    }
+    public DifferentialEquation(MathImplicitFunctionOperations rightSideFunction,
+                                String pathToParametersFile) throws IOException {
+        this(pathToParametersFile, rightSideFunction, null);
+    }
+    public DifferentialEquation (Point2D initialCondition, MathImplicitFunctionOperations rightSideFunction)
     {
         if (initialCondition.isNullPoint())
             throw new RuntimeException(PrettyOutput.ERROR +
                     "ОШИБКА! Для решения задачи Коши необходимо задать начальные условия" + PrettyOutput.RESET);
         this.rightSideFunction = rightSideFunction;
         this.initialCondition = initialCondition;
+    }
+    public HashMap<String, Double> getParametersTable() {
+        return parametersTable;
+    }
+    public double getEpsilon() {
+        return epsilon;
     }
     public MathImplicitFunctionOperations getRightSideFunction() {
         return rightSideFunction;
@@ -26,6 +81,14 @@ public class DifferentialEquation {
     public Point2D getInitialCondition() {
         return initialCondition;
     }
+    public void setParametersTable(String pathToParametersFile) throws IOException {
+        this.parametersTable.putAll(getVariablesTable(pathToParametersFile));
+    }
+    public void setEpsilon() {
+        if (this.parametersTable.containsKey("epsilon"))
+            if (this.parametersTable.get("epsilon") != null)
+                this.epsilon = parametersTable.get("epsilon");
+    }
     public void setRightSideFunction(MathImplicitFunctionOperations rightSideFunction) {
         this.rightSideFunction = rightSideFunction;
     }
@@ -34,6 +97,18 @@ public class DifferentialEquation {
     }
     public void setInitialCondition(Point2D initialCondition) {
         this.initialCondition = initialCondition;
+    }
+    public boolean isParameterOfMethodUpload(Collection<String> parameters)
+    {
+        for (String parameter : parameters)
+        {
+            if (!this.parametersTable.containsKey(parameter))
+                return false;
+            else
+                if (this.parametersTable.get(parameter) == null)
+                    return false;
+        }
+        return true;
     }
     public Point2D explicitAdamsMethod(Point2D prevApproximation, double step)
     {
@@ -51,26 +126,29 @@ public class DifferentialEquation {
                 currentApproximation.getX(), currentApproximation.getY()}, 2);
         double prevRightSideFunctionValue = this.rightSideFunction.calculatePoint(prevRightSideFunctionArguments).getY();
         double currRightSideFunctionValue = this.rightSideFunction.calculatePoint(currRightSideFunctionArguments).getY();
-        double nextApproximationValue = currentApproximation.getY() + (step / 2) *
+        double nextApproximationValue = prevApproximation.getY() + (step / 2) *
                 (currRightSideFunctionValue + prevRightSideFunctionValue);
         return new Point2D(currentApproximation.getX(), nextApproximationValue);
     } // ВЫЧИСЛЕНИЕ И КОРРЕКТИРОВКА ПРИБЛИЖЕНИЯ ДЛЯ НОВОГО ЗНАЧЕНИЯ РЕШЕНИЯ
     public void implicitAdamsMethod(String pathToFileWithParameters) throws IOException {
-        double stepOfMethod = 1;
-        double halfOfStep = stepOfMethod / 2;
-        double stepForCalculation = stepOfMethod;
-        double rightBorder = this.initialCondition.getX() + 5;
+        if (!this.isParameterOfMethodUpload(List.of("step of method", "right border")))
+        {
+            super.updateVariablesList(List.of("step of method", "right border"));
+            this.setParametersTable(pathToFileWithParameters);
+        }
+        double stepOfMethod = this.parametersTable.get("StepOfMethod".toLowerCase());
+        double rightBorder = this.parametersTable.get("RightBorder".toLowerCase());
         Stack<Point2D> functionApproximations = new Stack<>();
         functionApproximations.push(this.initialCondition.clonePoint());
-
-        for (double step = functionApproximations.peek().getX(); step < rightBorder; step += stepForCalculation)
+        for (double step = functionApproximations.peek().getX(); step < rightBorder; step += stepOfMethod)
         {
             Point2D prevApproximation = functionApproximations.peek();
-            Point2D currentApproximation = this.explicitAdamsMethod(prevApproximation, stepForCalculation);
-            /*for (int i = 0; i < 3; i++) {
-                currentApproximation = this.evaluateAndCorrect(prevApproximation, currentApproximation, stepOfMethod);
-            } // ИТЕРАЦИОННЫЙ ПРОЦЕСС ВЫЧИСЛЕНИЯ И КОРРЕКТИРОВКИ*/
-            Point2D nextApproximation = this.evaluateAndCorrect(prevApproximation, currentApproximation, stepForCalculation);
+            Point2D currentApproximation = this.explicitAdamsMethod(prevApproximation, stepOfMethod);
+            Point2D nextApproximation = this.evaluateAndCorrect(prevApproximation, currentApproximation, stepOfMethod);
+            do {
+                currentApproximation = nextApproximation.clonePoint();
+                nextApproximation = this.evaluateAndCorrect(prevApproximation, currentApproximation, stepOfMethod);
+            } while (Math.abs(nextApproximation.getY() - currentApproximation.getY()) >= this.epsilon);
             functionApproximations.push(nextApproximation);
         }
         this.solutionFunction = new MathFunctionOperations(new ArrayList<>(functionApproximations));
