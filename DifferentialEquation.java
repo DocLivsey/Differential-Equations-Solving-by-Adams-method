3b38a4d838aeeb59d09696cbb4a20206bf1b4ab9
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-05 18:21:39 +0300

    add new fields of class for the case of equations system
    change main constructor`s parameters
    and added new handlers of cases of input data
    
    adds hashCode method

--- DifferentialEquation.java
+++ DifferentialEquation.java
@@ -1,23 +1,42 @@
 import LinearAlgebra.*;
+import LinearAlgebra.Vector;
+import OtherThings.PrettyOutput;
 
 import java.io.*;
 import java.util.*;
 
 public class DifferentialEquation extends NumericalBase {
+
     protected MathImplicitFunctionOperations rightSideFunction; // F(x, y)
     protected MathFunctionOperations solutionFunction; // y(x)
-    public DifferentialEquation(String pathToParametersFile, String pathToPoints3D, MathImplicitFunction function,
-                                ArrayList<PointMultiD> points3D, MathImplicitFunctionOperations rightSideFunction)
+    protected boolean isEquationFromSystem = false;
+    protected Integer equationsIndex;
+    public DifferentialEquation(String pathToParametersFile, String pathToPointsMD, MathImplicitFunction function,
+                                ArrayList<PointMultiD> pointsMD, MathImplicitFunctionOperations rightSideFunction,
+                                boolean isEquationFromSystem, Integer equationsIndex, int dimension)
             throws IOException, ReflectiveOperationException {
         if (pathToParametersFile != null) {
             super.setFields(pathToParametersFile);
             this.setFields(pathToParametersFile);
-        }
-        if (rightSideFunction != null) {
+        } if (!this.isEquationFromSystem && !isEquationFromSystem) {
+            if (equationsIndex != null) {
+                this.isEquationFromSystem = true;
+                this.equationsIndex = equationsIndex;
+                throw new RuntimeException(PrettyOutput.ERROR + "Может это уравнение из системы?" + PrettyOutput.RESET);
+            }
+            this.equationsIndex = null;
+        } else {
+            if (equationsIndex == null) {
+                this.isEquationFromSystem = false;
+                this.equationsIndex = null;
+                throw new RuntimeException(PrettyOutput.ERROR + "Может это уравнение НЕ из системы?" + PrettyOutput.RESET);
+            }
+            this.equationsIndex = equationsIndex;
+        } if (rightSideFunction != null) {
             this.rightSideFunction = rightSideFunction;
         } else {
             this.rightSideFunction = new MathImplicitFunctionOperations(
-                    pathToParametersFile, pathToPoints3D, points3D, 2, function);
+                    pathToParametersFile, pathToPointsMD, pointsMD, isEquationFromSystem ? dimension : 2, function);
         }
     }
     public MathImplicitFunctionOperations getRightSideFunction() {
@@ -32,11 +51,15 @@ public class DifferentialEquation extends NumericalBase {
     public void setSolutionFunction(MathFunctionOperations solutionFunction) {
         this.solutionFunction = solutionFunction;
     }
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
     public CauchyProblemSolving setTheCauchyProblem(String pathToParametersFile)
             throws IOException, ReflectiveOperationException {
         return new CauchyProblemSolving(pathToParametersFile, this.rightSideFunction, null);
     }
-    public CauchyProblemSolving setTheCauchyProblem(Point2D initialCondition)
+    public CauchyProblemSolving setTheCauchyProblem(InitialCondition initialCondition)
             throws IOException, ReflectiveOperationException {
         return new CauchyProblemSolving(null, this.rightSideFunction, initialCondition);
     }
