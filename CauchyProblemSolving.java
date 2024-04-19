c0192e023ec39e40ad4bdf4497d517baf04e7c90
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-20 02:28:39 +0300

    add implicit adams algorithm multi dimension realisation

--- CauchyProblemSolving.java
+++ CauchyProblemSolving.java
@@ -83,33 +83,36 @@ public class CauchyProblemSolving extends DifferentialEquation {
     public void setInitialCondition(InitialCondition initialCondition) {
         this.initialCondition = initialCondition;
     }
-    public void setInitialCondition(String pathToParametersFile) throws IOException {
+    public void setInitialCondition(String pathToParametersFile) throws IOException, ReflectiveOperationException {
         HashMap<String, Double> initConditionCoordinatesTable = FileParser.SettingsParser.getParametersTable(pathToParametersFile);
         double x0 = initConditionCoordinatesTable.get("x0");
         double y0 = initConditionCoordinatesTable.get("y0");
-        this.initialCondition = new InitialCondition(x0, new Vector(new double[]{y0}, 1));
+        this.initialCondition = new InitialCondition(x0, new Vector(new ArrayList<>(List.of(y0))));
     }
     public Point2D explicitAdamsMethod(Point2D prevApproximation, double step)
-    {
-        Vector prevPointValues = new Vector(new double[]{
-                prevApproximation.getX(), prevApproximation.getY()}, 2);
+            throws ReflectiveOperationException, IOException {
+        Vector prevPointValues = new Vector(new ArrayList<>(List.of(
+                prevApproximation.getX(), prevApproximation.getY())));
         double prevRightSideFunctionValue = this.rightSideFunction.calculatePoint(prevPointValues).getY();
         return new Point2D(prevApproximation.getX() + step,
                 prevApproximation.getX() + step * prevRightSideFunctionValue);
     } // РАСЧЕТ НАЧАЛЬНОГО ПРИБЛИЖЕНИЯ ЯВНЫМ МЕТОДОМ АДАМСА ДЛЯ НЕЯВНОГО МЕТОДА АДАМСА
+
     public Point2D evaluateAndCorrect(Point2D prevApproximation, Point2D currentApproximation, double step)
-    {
-        Vector prevRightSideFunctionArguments = new Vector(new double[]{
-                prevApproximation.getX(), prevApproximation.getY()}, 2);
-        Vector currRightSideFunctionArguments = new Vector(new double[]{
-                currentApproximation.getX(), currentApproximation.getY()}, 2);
+            throws ReflectiveOperationException, IOException {
+        Vector prevRightSideFunctionArguments = new Vector(new ArrayList<>(List.of(
+                prevApproximation.getX(), prevApproximation.getY())));
+        Vector currRightSideFunctionArguments = new Vector(new ArrayList<>(List.of(
+                currentApproximation.getX(), currentApproximation.getY())));
         double prevRightSideFunctionValue = this.rightSideFunction.calculatePoint(prevRightSideFunctionArguments).getY();
         double currRightSideFunctionValue = this.rightSideFunction.calculatePoint(currRightSideFunctionArguments).getY();
         double nextApproximationValue = prevApproximation.getY() + (step / 2) *
                 (currRightSideFunctionValue + prevRightSideFunctionValue);
         return new Point2D(currentApproximation.getX(), nextApproximationValue);
     } // ВЫЧИСЛЕНИЕ И КОРРЕКТИРОВКА ПРИБЛИЖЕНИЯ ДЛЯ НОВОГО ЗНАЧЕНИЯ РЕШЕНИЯ
-    public void implicitAdamsMethod(String pathToFileWithParameters, String pathToSolutionsPointsOutput) throws IOException {
+
+    public void implicitAdamsMethod(String pathToFileWithParameters, String pathToSolutionsPointsOutput)
+            throws IOException, ReflectiveOperationException {
         if (!this.isParameterOfMethodUpload(List.of("step of method", "right border")))
         {
             super.updateVariablesList(List.of("step of method", "right border"));
@@ -184,12 +187,11 @@ public class CauchyProblemSolving extends DifferentialEquation {
             this(new ArrayList<>(){{
                 int dimension = mathImplicitFunctions.size() + 1;
                 for (int index = 0; index < dimension - 1; index++) {
-                    ArrayList<PointMultiD> points = new ArrayList<>(List.of(new PointMultiD(new Vector(new double[]{
-                            initialConditions.getX(), initialConditions.getY(index)}, dimension),
-                            mathImplicitFunctions.get(index).function(initialConditions.convertToVector()).getY())));
-                    this.add(new DifferentialEquation(pathToParametersFile, null,
-                            mathImplicitFunctions.get(index), points, null,
-                            true, index, dimension));
+                    ArrayList<PointMultiD> points = new ArrayList<>(List.of(new PointMultiD(initialConditions
+                            .convertToVector(), mathImplicitFunctions.get(index).function(initialConditions
+                            .convertToVector()).getY())));
+                    this.add(new DifferentialEquation(pathToParametersFile, null, mathImplicitFunctions
+                            .get(index), points, null, true, index, dimension));
                 }
             }}, pathToParametersFile, initialConditions);
         }
@@ -208,6 +210,105 @@ public class CauchyProblemSolving extends DifferentialEquation {
         public void setInitialConditions(String pathToParametersFile) throws IOException {
             HashMap<String, Double> initConditionCoordinatesTable = FileParser.SettingsParser.getParametersTable(pathToParametersFile);
         }
+
+        public ArrayList<Point2D> explicitAdamsMethod(ArrayList<Point2D> prevApproximations, double step)
+                throws ReflectiveOperationException, IOException {
+            Vector prevPointsValues = new Vector(){{
+                prevApproximations.forEach(point -> this.addElement(point.getY()));
+            }};
+            Vector prevRightSideFunctionValue = new Vector(){{
+                for (int index = 0; index < differentialEquationsSystem.size(); index++) {
+                    this.addElement(differentialEquationsSystem.get(index).rightSideFunction.calculatePoint(
+                            prevApproximations.get(index).toVector()).getY());
+                }
+            }};
+            Vector nextApproximationsValues = (Vector) prevPointsValues
+                    .add(prevRightSideFunctionValue.constMultiply(step));
+            return new ArrayList<>(){{
+                nextApproximationsValues.getVector().forEach(value -> this.add(new Point2D(
+                        prevApproximations.get(0).getX() + step, value)));
+            }};
+        } // РАСЧЕТ НАЧАЛЬНОГО ПРИБЛИЖЕНИЯ ЯВНЫМ МЕТОДОМ АДАМСА ДЛЯ НЕЯВНОГО МЕТОДА АДАМСА
+
+        public ArrayList<Point2D> evaluateAndCorrect(
+                ArrayList<Point2D> prevApproximations, ArrayList<Point2D> currentApproximations, double step)
+                throws ReflectiveOperationException, IOException {
+            Vector prevRightSideFunctionArguments = new Vector(new ArrayList<>(){{
+                prevApproximations.forEach(point -> {
+                    try {
+                        point.toVector();
+                    } catch (ReflectiveOperationException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }});
+            Vector currRightSideFunctionArguments = new Vector(new ArrayList<>(){{
+                currentApproximations.forEach(point -> {
+                    try {
+                        point.toVector();
+                    } catch (ReflectiveOperationException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }});
+            Vector prevRightSideFunctionValue = new Vector(new ArrayList<>(){{
+                for (int index = 0; index < differentialEquationsSystem.size(); index++) {
+                    this.add(differentialEquationsSystem.get(index).rightSideFunction.calculatePoint(
+                            prevApproximations.get(index).toVector()).getY());
+                }
+            }});
+            Vector currRightSideFunctionValue = new Vector(new ArrayList<>(){{
+                for (int index = 0; index < differentialEquationsSystem.size(); index++) {
+                    this.add(differentialEquationsSystem.get(index).rightSideFunction.calculatePoint(
+                            currentApproximations.get(index).toVector()).getY());
+                }
+            }});
+            Vector nextApproximationValue = (Vector) prevRightSideFunctionValue.add(currRightSideFunctionValue
+                    .add(prevRightSideFunctionValue).constMultiply(step / 2));
+            return new ArrayList<>(){{
+               nextApproximationValue.getVector().forEach(value -> this.add(new Point2D(
+                       currentApproximations.get(0).getX(), value)));
+            }};
+        } // ВЫЧИСЛЕНИЕ И КОРРЕКТИРОВКА ПРИБЛИЖЕНИЯ ДЛЯ НОВОГО ЗНАЧЕНИЯ РЕШЕНИЯ
+
+        public void equationsSolving(String pathToFileWithParameters, String pathToSolutionsPointsOutput)
+                throws IOException, ReflectiveOperationException {
+            int maxIterationsCount = 1000;
+            if (!this.isParameterOfMethodUpload(List.of("step of method", "right border")))
+            {
+                super.updateVariablesList(List.of("step of method", "right border"));
+                this.setParametersTable(pathToFileWithParameters);
+            }
+            double rightBorder = this.parametersTable.get("RightBorder".toLowerCase());
+            double stepOfMethod = this.parametersTable.get("StepOfMethod".toLowerCase());
+            Stack<ArrayList<Point2D>> functionApproximations = new Stack<>(){{
+                ArrayList<Point2D> solutionsPoints = new ArrayList<>();
+                for (int i = 0; i < differentialEquationsSystem.size(); i++)
+                    solutionsPoints.add(new Point2D(initialConditions.getX(), initialConditions.getY(i)));
+                this.push(solutionsPoints);
+            }};
+            for (double step = functionApproximations.peek().get(0).getX(); step < rightBorder; step += stepOfMethod) {
+                ArrayList<Point2D> prevApproximations = functionApproximations.peek();
+                ArrayList<Point2D> currentApproximations = this.explicitAdamsMethod(prevApproximations, stepOfMethod);
+                ArrayList<Point2D> nextApproximations = this.evaluateAndCorrect(prevApproximations,
+                        currentApproximations, stepOfMethod);
+                int iterations = 0;
+                do {
+                    iterations++;
+                    currentApproximations = new ArrayList<>(nextApproximations);
+                    nextApproximations = this.evaluateAndCorrect(prevApproximations, currentApproximations, stepOfMethod);
+                } while(iterations < maxIterationsCount);
+                functionApproximations.push(nextApproximations);
+            }
+            System.out.println(functionApproximations);
+            for (int equationIndex = 0; equationIndex < this.differentialEquationsSystem.size(); equationIndex++)
+            {
+                this.differentialEquationsSystem.get(equationIndex).getSolutionFunction()
+                        .writePointsInFile("SolutionsFunction" + equationIndex + "PointsOutput.txt");
+            }
+        }
+
+
         public void implicitAdamsMethod(String pathToFileWithParameters, String pathToSolutionsPointsOutput)
                 throws Exception {
             if (!this.isParameterOfMethodUpload(List.of("step of method", "right border")))
@@ -218,7 +319,7 @@ public class CauchyProblemSolving extends DifferentialEquation {
             double stepOfMethod = this.parametersTable.get("StepOfMethod".toLowerCase());
             double finalRightBorder = this.parametersTable.get("RightBorder".toLowerCase());
             double localRightBorder = this.initialConditions.getX() + stepOfMethod;
-            InitialCondition localInitCondition = this.initialConditions.cloneInitCondition();
+            InitialCondition localInitCondition = this.initialConditions.copy();
             while (localRightBorder <= finalRightBorder)
             {
                 for (int equationIndex = 0; equationIndex < this.differentialEquationsSystem.size(); equationIndex++)
@@ -253,19 +354,6 @@ public class CauchyProblemSolving extends DifferentialEquation {
                 this.differentialEquationsSystem.get(equationIndex).getSolutionFunction()
                         .writePointsInFile("SolutionsFunction" + equationIndex + "PointsOutput.txt");
             }
-        }
-
-        public void systemSolving(String pathToFileWithParameters, String pathToSolutionsPointsOutput)
-                throws IOException, ReflectiveOperationException {
-            if (!this.isParameterOfMethodUpload(List.of("step of method", "right border")))
-            {
-                super.updateVariablesList(List.of("step of method", "right border"));
-                this.setParametersTable(pathToFileWithParameters);
-            }
-            int iterations = 0;
-            double stepOfMethod = this.parametersTable.get("StepOfMethod".toLowerCase());
-            double finalRightBorder = this.parametersTable.get("RightBorder".toLowerCase());
-
         }
     }
 }
